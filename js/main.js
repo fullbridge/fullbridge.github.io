96d403e73e25f93795660e72102d50e5911d9089
Mike Mai <emceehao@gmail.com>
2015-06-29 12:57:33 -0400

    Utilities nav

--- js/main.js
+++ js/main.js
@@ -18,40 +18,32 @@ $(document).ready(function() {
     });
 });
 
-/* Range input click to update output value (it only catches the first range input on the page, need re-factoring before production). */
+/* Open split buttons and tooltips (doesn't always work on mobile, something is delaying the action) */
 
-if (document.querySelector) {
-    document.querySelector(".range").onchange = function(e) {
-        e.target.nextElementSibling.innerText = e.target.value;
-    }
-}
-
-/* Open split buttons and tooltips */
-
-$(".drop, .tooltip").find(".link, .button, .tip").click(function(e) {
-    $(e.target).toggleClass("open")
+$(".drop, .tooltip").children(".link, .button, .tip").click(function(e) {
+    $(this).toggleClass("open")
 })
 
 $(document).find("[data-tip]").click(function(e) {
-    $(e.target).toggleClass("open")
+    $(this).toggleClass("open")
 })
 
 /* Close split buttons and tooltips when clicked outside (does not work on mobile, need re-factoring before production). http://stackoverflow.com/questions/1403615/use-jquery-to-hide-a-div-when-the-user-clicks-outside-of-it */
 
 $(document).mouseup(function (e)
 {
-    var container = $(".link, .button, .tip");
+    var container = $(".drop, .tooltip");
 
     if (!container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
     {
-        container.removeClass("open");
+        container.children(".link, .button, .tip").removeClass("open");
     }
 });
 
-// in HTML <head>:
+// This makes the .tabs work in IE8 and below, add in HTML <head>:
 // <!--[if lt IE 9]><script>window.ltIE9=true</script><![endif]-->
-if(window.ltIE9) {
+/* if(window.ltIE9) {
   (function($) {
     $('.tabs input[type="radio"]:checked')
     .closest('.tab')
@@ -65,4 +57,12 @@ if(window.ltIE9) {
       .addClass('checked');
     });
   })(window.jQuery);
-}
+} */
+
+/* Range input click to update output value (it only catches the first range input on the page, and it returns error when there's no range input on the page, need re-factoring before production).
+
+if (document.querySelector) {
+    document.querySelector(".range").onchange = function(e) {
+        e.target.nextElementSibling.innerText = e.target.value;
+    }
+} */
