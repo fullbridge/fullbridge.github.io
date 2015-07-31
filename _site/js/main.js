36307435b316c0007f363a51765e44aa37974a01
Mike Mai <emceehao@gmail.com>
2015-07-31 11:32:18 -0400

    Logo assets

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,152 +1,152 @@
-// Fast detect scroll on mobile.
 
-$(document).on('touchstart touchend touchmove', function(){ $(window).trigger('scroll'); });
+$(document).ready(function() {
+  // Fast detect scroll on mobile.
+  $(document).on('touchstart touchend touchmove', function(){ $(window).trigger('scroll'); });
 
-// Hide subnav on scroll down, show on scroll up.
+  // Hide subnav on scroll down, show on scroll up.
 
-$(function(){
-    var nav = $(".subnav.fixed"),
-        state = "down", 
-        lastScrollTop = 0, delta = 100;
+  $(function(){
+      var nav = $(".subnav.fixed"),
+          state = "down", 
+          lastScrollTop = 0, delta = 100;
 
-    $(window).scroll(function(event){
-      var st = $(this).scrollTop();
-       
-      if(Math.abs(lastScrollTop - st) <= delta){return};
+      $(window).scroll(function(event){
+        var st = $(this).scrollTop();
+         
+        if(Math.abs(lastScrollTop - st) <= delta){return};
 
-      if ((st > lastScrollTop) && (lastScrollTop>0)) {
-         // downscroll code
-         if(state == "up"){
-          $(".subnav.fixed").addClass("scrolled");
-          state = "down";
+        if ((st > lastScrollTop) && (lastScrollTop>0)) {
+           // downscroll code
+           if(state == "up"){
+            $(".subnav.fixed").addClass("scrolled");
+            state = "down";
+          }
+        } else {
+          // upscroll code
+          if(state == "down"){
+          $(".subnav.fixed").removeClass("scrolled");
+          state = "up";
+          }
         }
+          lastScrollTop = st;
+      });
+  });
+
+  // Maximize sidebar when page is scrolled.
+
+  $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 1) {
+          $(".sidebar.fixed").addClass("scrolled");
       } else {
-        // upscroll code
-        if(state == "down"){
-        $(".subnav.fixed").removeClass("scrolled");
-        state = "up";
-        }
+          $(".sidebar.fixed").removeClass("scrolled");
       }
-        lastScrollTop = st;
-    });
-});
+  });
 
-// Maximize sidebar when page is scrolled.
+  // Maximize sidebar when page is scrolled.
 
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+  $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
 
-    if (scroll >= 1) {
-        $(".sidebar.fixed").addClass("scrolled");
-    } else {
-        $(".sidebar.fixed").removeClass("scrolled");
-    }
-});
+      if (scroll >= 600) {
+          $(".steps-nav").addClass("scrolled");
+      } else {
+          $(".steps-nav").removeClass("scrolled");
+      }
+  });
 
-// Maximize sidebar when page is scrolled.
+  // Open split buttons and tooltips (doesn't always work on mobile, something is delaying the action for anchor and button elements, need re-factoring before production).
 
-$(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
+  $(".drop, .tooltip").children(".link, .button, .tip").click(function(e) {
+      $(".link, .button, .tip").not(this).removeClass("open")
+      $(this).toggleClass("open")
+  })
 
-    if (scroll >= 600) {
-        $(".steps-nav").addClass("scrolled");
-    } else {
-        $(".steps-nav").removeClass("scrolled");
-    }
-});
+  $(document).find("[data-tip], .side-nav>header").click(function(e) {
+      $("[data-tip], .side-nav>header").not(this).removeClass("open")
+      $(this).toggleClass("open")
+  })
 
-// Open split buttons and tooltips (doesn't always work on mobile, something is delaying the action for anchor and button elements, need re-factoring before production).
+  // Close split buttons and tooltips when clicked outside (does not work on mobile, need re-factoring before production). http://stackoverflow.com/questions/1403615/use-jquery-to-hide-a-div-when-the-user-clicks-outside-of-it
 
-$(".drop, .tooltip").children(".link, .button, .tip").click(function(e) {
-    $(".link, .button, .tip").not(this).removeClass("open")
-    $(this).toggleClass("open")
-})
+  $(document).mouseup(function (e)
+  {
+      var container = $(".drop, .tooltip");
 
-$(document).find("[data-tip], .side-nav>header").click(function(e) {
-    $("[data-tip], .side-nav>header").not(this).removeClass("open")
-    $(this).toggleClass("open")
-})
+      if (!container.is(e.target) // if the target of the click isn't the container...
+          && container.has(e.target).length === 0) // ... nor a descendant of the container
+      {
+          container.children(".link, .button, .tip").removeClass("open");
+      }
+  });
 
-// Close split buttons and tooltips when clicked outside (does not work on mobile, need re-factoring before production). http://stackoverflow.com/questions/1403615/use-jquery-to-hide-a-div-when-the-user-clicks-outside-of-it
+  // Close reveal modals.
 
-$(document).mouseup(function (e)
-{
-    var container = $(".drop, .tooltip");
+  $(".reveal-modal, .overlay").find(".close").click(function(e) {
+      $(".reveal-modal, .overlay").removeClass("open")
+  })
 
-    if (!container.is(e.target) // if the target of the click isn't the container...
-        && container.has(e.target).length === 0) // ... nor a descendant of the container
-    {
-        container.children(".link, .button, .tip").removeClass("open");
-    }
-});
+  // Form field validates after focus once.
 
-// Close reveal modals.
+  $(':required').on('blur keydown', function() {
+      $(this).addClass('touched');
+  });
 
-$(".reveal-modal, .overlay").find(".close").click(function(e) {
-    $(".reveal-modal, .overlay").removeClass("open")
-})
+  // Make label visible after value is entered.
 
-// Form field validates after focus once.
-
-$(':required').on('blur keydown', function() {
-    $(this).addClass('touched');
-});
-
-// Make label visible after value is entered.
-
-$('input,textarea,select').on('change', function() {
-    $(this).addClass('filled')
-})
-
-$(document).ready(function() {
-    $("input, textarea").each(function(i, element) {
-        if ($.trim($(element).val()) != "") {
-            $(element).addClass("filled");
-        }
-    });
-});
-
-// truncate long text.
-
-var boxContentFullText = [];
-var boxContentCutoffLength = 80;
+  $('input,textarea,select').on('change', function() {
+      $(this).addClass('filled')
+  })
 
 
-function displayFullText(index) {
-       $(".truncate").html(boxContentFullText[index]);
-}
+      $("input, textarea").each(function(i, element) {
+          if ($.trim($(element).val()) != "") {
+              $(element).addClass("filled");
+          }
+      });
 
-$(document).ready(function() {
-       $.each($(".truncate"), function(index, el){
-               current = $(el);
-               if (current.html().length >  boxContentCutoffLength) {
-                       boxContentFullText[index] = current.html();
-                       current.html(current.html().substr(0,boxContentCutoffLength-1) + "&hellip; <small class='nowrap'><a href='javascript:displayFullText(" + index + ")';>show</a></small class='nowrap'>");
-               }
-       });
-});
 
-// This makes the .tabs work in IE8 and below, add in HTML <head>:
-// <!--[if lt IE 9]><script>window.ltIE9=true</script><![endif]-->
-/* if(window.ltIE9) {
-  (function($) {
-    $('.tabs input[type="radio"]:checked')
-    .closest('.tab')
-    .addClass('checked');
-    $('html').on('click', '.tabs input', function() {
-      $('input[name="' + this.name + '"]')
-      .closest('.tab')
-      .removeClass('checked');
-      $(this)
+  // truncate long text.
+
+  var boxContentFullText = [];
+  var boxContentCutoffLength = 80;
+
+
+  function displayFullText(index) {
+         $(".truncate").html(boxContentFullText[index]);
+  }
+
+   $.each($(".truncate"), function(index, el){
+           current = $(el);
+           if (current.html().length >  boxContentCutoffLength) {
+                   boxContentFullText[index] = current.html();
+                   current.html(current.html().substr(0,boxContentCutoffLength-1) + "&hellip; <small class='nowrap'><a href='javascript:displayFullText(" + index + ")';>show</a></small class='nowrap'>");
+           }
+   });
+
+  // This makes the .tabs work in IE8 and below, add in HTML <head>:
+  // <!--[if lt IE 9]><script>window.ltIE9=true</script><![endif]-->
+  /* if(window.ltIE9) {
+    (function($) {
+      $('.tabs input[type="radio"]:checked')
       .closest('.tab')
       .addClass('checked');
-    });
-  })(window.jQuery);
-} */
+      $('html').on('click', '.tabs input', function() {
+        $('input[name="' + this.name + '"]')
+        .closest('.tab')
+        .removeClass('checked');
+        $(this)
+        .closest('.tab')
+        .addClass('checked');
+      });
+    })(window.jQuery);
+  } */
 
-//Range input click to update output value (it only catches the first range input on the page, and it returns error when there's no range input on the page, need re-factoring before production).
-/* if (document.querySelector) {
-    document.querySelector(".range").onchange = function(e) {
-        e.target.nextElementSibling.innerText = e.target.value;
-    }
-} */
+  //Range input click to update output value (it only catches the first range input on the page, and it returns error when there's no range input on the page, need re-factoring before production).
+  /* if (document.querySelector) {
+      document.querySelector(".range").onchange = function(e) {
+          e.target.nextElementSibling.innerText = e.target.value;
+      }
+  } */
+});
