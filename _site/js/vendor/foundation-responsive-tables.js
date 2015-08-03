c08f65e720a654242663c63695c41b309d1541d4
Mike Mai <emceehao@gmail.com>
2015-08-03 16:33:32 -0400

    Profile photos

--- _site/js/vendor/foundation-responsive-tables.js
+++ _site/js/vendor/foundation-responsive-tables.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
   var switched = false;
   var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
+    if (($(window).width() < 3000) && !switched ){
       switched = true;
       $("table.responsive").each(function(i, element) {
         splitTable($(element));
       });
       return true;
     }
-    else if (switched && ($(window).width() > 767)) {
+    else if (switched && ($(window).width() > 3000)) {
       switched = false;
       $("table.responsive").each(function(i, element) {
         unsplitTable($(element));
